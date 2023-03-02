c1c3001646891c52dca6a613fac02a1f9bb24e03
Efe Agare <knowledgeagare157@gmail.com>
2023-03-02 15:40:50 +0100

    add the spec helpers

---
+++ spec/factories/employees.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :employee do
+    name { "Agare" }
+    department
+  end
+end
---
+++ spec/factories/mystery_lunch_partners.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :mystery_lunch_partners do
+    mystery_lunch
+    employee
+  end
+end
